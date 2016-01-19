8e718f223db90135f708ce7e6a91460488176995
Dominic Baggott <dominic.baggott@gmail.com>
2016-01-19 15:00:09 +0000

    Load REDIS_URL environment variable in config.rb

--- lib/config.rb
+++ lib/config.rb
@@ -1,3 +1,6 @@
 # For a Github Application: https://github.com/settings/applications/new
 GITHUB_CLIENT_ID = ENV.fetch('GH_BASIC_CLIENT_ID')
 GITHUB_CLIENT_SECRET = ENV.fetch('GH_BASIC_SECRET_ID')
+
+# Redis connection URL
+REDIS_URL = ENV.fetch('REDIS_URL', 'redis://localhost:6379')
